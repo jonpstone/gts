7efc3f04a53487041d860742d79d754d2f38f624
Jonathan Stone <jpstone@protonmail.com>
2019-09-18 23:44:35 -0500

    Added text to about section for About page.

--- src/components/singlePages/About.js
+++ src/components/singlePages/About.js
@@ -4,5 +4,14 @@ import { Container } from 'semantic-ui-react'
 
 export default props =>
   <Container text style={{ marginTop: '7em' }}>
-    About
+    <h2>About<h1>
+    <p>
+      Golbourn Training Solutions believes in making training fun, interactive and realistic. With our courses the dreaded mandatory training is a thing of the past. We will adapt our courses around each and every learner to maximise not only their individual experience but for the group as a whole. The right environment, the right atmosphere and the right instructor can get the best out of everyone. 
+      
+      We make our First Aid Training scenarios as realistic as possible with the use of casualty simulation, this is not only fun and realistic, but it\'s a crucial learning tool. When your First Aider is in an emergency situation their training and muscle memory will make the difference.
+      
+      Learning is developing, developing is investing, investing is securing success for the future. 
+      
+      We aim to help make your staff the best they can be, and with our security training we will prepare you with the skills to protect your staff and clients.
+    </p>
   </Container>
