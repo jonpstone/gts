48e2ace60d7928a82eb632d1ad1f26b321eb2d7d
Jonathan Stone <jpstone@protonmail.com>
2019-09-18 23:48:49 -0500

    Updated text to Forces Covenant

--- src/components/singlePages/ForcesCovenant.js
+++ src/components/singlePages/ForcesCovenant.js
@@ -3,5 +3,11 @@ import { Container } from 'semantic-ui-react'
 
 export default props => 
   <Container text style={{ marginTop: '7em' }}>
-    Forces Covenant
-  </Container>
+    Whether you are leaving the Armed Forces or your direct family would like to increase their employability, Golbourn Training Solutions are passionate in helping our Armed Forces and their families.
+
+    We work with serving personnel (Regular and Reserves), Veterans and direct family members.
+
+    All Armed Forces can access any one of our public courses at a discounted rate or can attend one of our specialised MOD Courses where we cover multiple subjects over 3 days.  We teach nationwide and can teach on MOD bases, including ATCs if requested.
+
+    Please contact us for further details.
+   </Container>
