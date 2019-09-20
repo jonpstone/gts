40802cf3172c6fff933a6f074646a022a289dcb1
Jonathan Stone <jpstone@protonmail.com>
2019-09-19 23:46:31 -0500

    Updated Auditing page.

--- src/components/singlePages/Auditing.js
+++ src/components/singlePages/Auditing.js
@@ -4,5 +4,8 @@ import { Container } from 'semantic-ui-react'
 
 export default props =>
   <Container text style={{ marginTop: '7em' }}>
-    Auditing COMING SOON
+    <h1>Auditing</h1>
+    <p>
+      Coming soon to Golbourn Training Solutions
+    </p>
   </Container>
