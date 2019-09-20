2bbc729a6930fb89a25fd42b42df136222b6789c
Jonathan Stone <jpstone@protonmail.com>
2019-09-20 02:33:15 -0500

    Updated JSX for TeamBuilding.

--- src/components/singlePages/TeamBuilding.js
+++ src/components/singlePages/TeamBuilding.js
@@ -4,5 +4,101 @@ import { Container } from 'semantic-ui-react'
 
 export default props =>
   <Container text style={{ marginTop: '7em' }}>
-    Team Building
+    <h1>Team Building & Events</h1>
+    <p>
+      Golbourn Training Solutions has many years’ experience in organising different team building exercises and 
+      events, from walking Hadrian’s Wall over 6 days to developing groups communication skills.
+    </p>
+
+    <h1>OUR EXPERT EVENT MANAGER FRED BOUCHER IS SECOND TO NONE – </h1>
+    <h2>your staff will be energised and motivated to achieve results</h2>
+
+    <p>
+      <b>Regular asked questions:</b>
+      You can have an event run at your location or we can provide a suitable venue.
+      You can pay per head or for an overall booking. We will build an event to meet your needs.
+      You can incorporate the event into your companies training packages to suit your message.
+    </p>
+
+    <h2>WOODLAND SURVIVAL COURSE</h2> - Bring your team together in this exciting outdoor experience, they will develop new skills and learn dynamic ways to work together to solve any task:
+    
+    <ul>
+      <li>Building Shelter</li>
+      <li>Making Fire</li>
+      <li>Purifying Water</li>
+      <li>Hunting Food</li>
+      <li>Navigation</li>
+      <li>Team Command Tasks</li>
+      <li>Assualt Courses (optional)</li>
+      <li>Problem Solving</li>
+    </ul>
+
+    <p>
+      Our new Survival Skills and Command Tasks, run by our survival expert Fred Boucher will bring your teams together 
+      to problem solve outside of the office environment.  These can be run over a few hours or over night.  A fun, 
+      active, action packed experience awaits all that are courageous enough to take on this opportunity. A tried and 
+      tested technique of developing a groups cohesion and communication abilities are group activities both physical 
+      and mental.
+    </p>
+
+    <h1>CORPORATE TEAM BUILDING EVENTS</h1> 
+
+    <p>
+      – Tailored to meet your bespoke needs for all group sizes and abilities. Our team will build you the package to 
+      motivate and inspire your staff. You tell us what you want to achieve and we will do the rest.
+    </p>
+
+    <ul>
+      <li>The Maze</li>
+      <li>Who did it</li>
+      <li>Army challenge</li>
+      <li>The Den</li>
+      <li>Many Many More</li> 
+    </ul>
+
+    <h1>Contact Us Today: 01926 825682</h1>
+
+    <h1>Some of our past events</h1>
+
+    <h3>TEAM BUILDING WALKS WITH GOLBOURN TRAINING SOLUTIONS:</h3>
+
+    <b>Walk Hadrian’s wall - Empires End. (6 days)</b>
+    <p>
+      A group walk to truly test the limits of your staff, 82 miles in 6 days across 
+      diverse terrain, carrying our party’s own tent, equipment, food and water. 
+      Although this is not for the faint hearted, the bond and team spirit your 
+      staff will gain from this experience will only be matched by their sense of 
+      overwhelming achievement. You will be walking in history’s footsteps, our 
+      instructor guides will be informing you of the historical facts of key moments in the Walls history and events, the 
+      units that manned the mile forts and where these men came from.
+    </p>
+
+    <b>Experience Hadrian’s wall - Hadrian’s Gift. (5 days)</b>
+    <p>
+      This adventure is far less demanding on the feet than Empires End Walk, but still a life experience never to be 
+      forgotten. This 5-day event will see you and your staff mini bused around the spectacular roman museums along the 
+      length off the wall, while enjoying the local hospitality from tea rooms to traditional overnight accommodation. 
+      However, you won’t miss out on the iconic views of the pennines section of the wall, snaking through the cliffs and 
+      see the wall tree made famous in the Kevin costner film Robin Hood the Prince of Thieves. This 21-mile walk is slow 
+      time, allowing you to take in the beautiful landscapes. This event will finish with a well-deserved evening out in 
+      Newcastle.
+    </p>
+
+    <b>Battlefield Tour - Battle of Edgehill. (1 day)</b>
+    <p>
+      This 15-mile walk will see you walk through some of the most amazing hill views the midlands has to offer, covering 
+      the iconic Burton Dassett and Edgehill hills. On your travels you and your staff will learn about the murderous local 
+      history of the trails as well as stop at some of the oldest Warwickshire pubs for refreshments and lunch. After 
+      dinner a period dressed matter expert in the history of the Battle of Edgehill will give a detailed account from the 
+      view point of which the King stood to observe the battle, detailing where the units formed and engaged each other. 
+      You will also be able to handle period weapons and equipment during this talk.
+    </p>
+
+    <b>Other walks and events:</b>
+
+    Battlefield Tour - Battle of Bosworth. (1day)
+
+    Cotswold Way - Chipping Campden to Bath. (4 days)
+
+    Warwickshire Military Museum Tours. (3 days)
   </Container>
