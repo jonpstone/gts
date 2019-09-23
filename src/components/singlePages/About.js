f185cc074edfcadfbb79e2f7c61f3e6c74ede347
Jonathan Stone <jpstone@protonmail.com>
2019-09-23 00:38:14 -0500

    Added semantic UI tags to About.

--- src/components/singlePages/About.js
+++ src/components/singlePages/About.js
@@ -4,14 +4,46 @@ import { Container } from 'semantic-ui-react'
 
 export default props =>
   <Container text style={{ marginTop: '7em' }}>
-    <h2>About<h1>
+    <Header='h2'>About</Header>
     <p>
-      Golbourn Training Solutions believes in making training fun, interactive and realistic. With our courses the dreaded mandatory training is a thing of the past. We will adapt our courses around each and every learner to maximise not only their individual experience but for the group as a whole. The right environment, the right atmosphere and the right instructor can get the best out of everyone. 
+      Golbourn Training Solutions believes in making training fun, interactive and realistic. With our courses the dreaded mandatory 
+      training is a thing of the past. We will adapt our courses around each and every learner to maximise not only their individual 
+      experience but for the group as a whole. The right environment, the right atmosphere and the right instructor can get the best 
+      out of everyone. 
       
-      We make our First Aid Training scenarios as realistic as possible with the use of casualty simulation, this is not only fun and realistic, but it is a crucial learning tool. When your First Aider is in an emergency situation their training and muscle memory will make the difference.
+      We make our First Aid Training scenarios as realistic as possible with the use of casualty simulation, this is not only fun and 
+      realistic, but it is a crucial learning tool. When your First Aider is in an emergency situation their training and muscle memory 
+      will make the difference.
       
       Learning is developing, developing is investing, investing is securing success for the future. 
       
-      We aim to help make your staff the best they can be, and with our security training we will prepare you with the skills to protect your staff and clients.
+      We aim to help make your staff the best they can be, and with our security training we will prepare you with the skills to protect
+      your staff and clients.
+    </p>
+    <Header='h2'>Our Team</Header>
+    <p>
+      Steven Golbourn
+      Managing Director
+      Phone: 07904 014772 - 01926825682
+      E-mail: steve@golbourntrainingsolutions.co.uk
+
+      My name is Steve, and I’m the Managing Director of Golbourn Training Solutions and lead instructor, I started this company to not 
+      only make a successful business, but to enhance a part of my character and life that I love, investing and developing other 
+      people’s skills. Watching a learner go from strength to strength over a short period of time truly does motivate me.
+
+      My background in instructing and development has been gained from 16 years’ service in the British Army, where I was a Team Medic, 
+      Battlefield Casualty Drills Trainer and Small Arms Weapons Instructor. I have completed multiple operational tours and met many 
+      wonderful people from all walks of life and parts of the world. The military has given me the skills to be dynamic, flexible and 
+      have confidence in my holistic approach to learner development. I have a can do, will do attitude.
+
+      The SICM - Serious Incident Control Measures Course was designed to help organisations to be able to impliment their own easy step 
+      by step protection system, while ensuring the staff are as prepared and practiced as possible to deal with terror threats. Have a 
+      look at our SICM page for more information, including our Lockdown Services. 
+
+      I aim to provide competitive prices to all my courses, but Golbourn Training Solutions openly offers discounts and support to the 
+      Armed Forces, Blue Light Services, Schools, Charities, Care Industries and Volunteer groups. Some of the UK's best.
+
+      I hope you select Golbourn Training Solutions to help in your investment and development of your staff now and in the future. 
+      I aim to prove compliance training can be fun.
     </p>
   </Container>
