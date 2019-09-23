f54692a4f598cd3efea795c763ccaa0d351e83c2
Jonathan Stone <jpstone@protonmail.com>
2019-09-22 23:50:52 -0500

    Headers looked weird, rectified missing right greater than.

--- src/components/singlePages/Home.js
+++ src/components/singlePages/Home.js
@@ -4,25 +4,25 @@ import { Container, Header} from 'semantic-ui-react'
 export default props =>
   <React.Fragment>
     <Container text style={{ marginTop: '7em' }}>
-      <Header as='h2'Compliance</Header>
+      <Header as='h2'>Compliance</Header>
       <p>
         Including First Aid, Mental Health First Aid, Health & Safety, Safeguarding, Fire Safety, Manual Handing and Food Safety.
       </p>
     </Container>
     <Container text style={{ marginTop: '7em' }}>
-      <Header as='h2'Organisation Security</Header>
+      <Header as='h2'>Organisation Security</Header>
       <p>
         Threat analysis, Lockdown, Serious Incident control Measures and Serious Injury First Aid.
       </p>
     </Container>
     <Container text style={{ marginTop: '7em' }}>
-      <Header as='h2'Auditing</Header>
+      <Header as='h2'>Auditing</Header>
       <p>
         Preparing organisations to achieve ISO 9001 Certification in Quality Management
       </p>
     </Container>
     <Container text style={{ marginTop: '7em' }}>
-      <Header as='h2'Team Building</Header>
+      <Header as='h2'>Team Building</Header>
       <p>
         Every programme is tailor made to the clients needs.  Corporate Team Building, Woodland Survival Team Building.
       </p>
