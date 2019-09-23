2c0be2f6ca2e5f2474c08bf75aee9fe4b79622f4
Jonathan Stone <jpstone@protonmail.com>
2019-09-22 19:47:56 -0500

    Added initial text to OrgSec.js

--- src/components/singlePages/OrganisationSecurity.js
+++ src/components/singlePages/OrganisationSecurity.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 const OrgSec = () => (
   <div className="orgSec">
-
+    <h2>Security Audit</h2>
+    <p>
+    	A review of your current security measures and processes and a report of our findings.
+    </p>
+    <h2>Lockdown Consultancy</h2>
+    <p>
+    	Consultancy and training to ensure your orgainisation is Lockdown ready.
+    </p>
+    <h2>Serious Incident Control Measures</h2>
+    <p>
+      As a Nationally registered CPD qualification, our Serious Incident Control Measures course 
+      will give the learner the training to identify security risks and manage security incidents.  
+      This course also includes serious injury first aid.
+    </p>
   </div>
 );
 
