b6418ded225da1e53e31e4dc9773cb2deaaa3602
Jonathan Stone <jpstone@protonmail.com>
2019-09-22 20:40:47 -0500

    Updated Public Courses.

--- src/components/singlePages/PublicCourses.js
+++ src/components/singlePages/PublicCourses.js
@@ -4,5 +4,11 @@ import { Container } from 'semantic-ui-react'
 
 export default props =>
   <Container text style={{ marginTop: '7em' }}>
-    Public Courses
+    <h2>Public Courses Dates</h2>
+    <p>
+      See Golbourn Training Solutions Managing Directors training availability, please keep in mind any date and location 
+      is possible through the Regional Instructors.
+      
+      Below are a list of all our Public Courses, we also deliver private courses please contact us for more details.
+    </p>
   </Container>
