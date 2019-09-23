2f02b3d3269b2d8a733ed2a829d7609d945a2e94
Jonathan Stone <jpstone@protonmail.com>
2019-09-22 23:30:42 -0500

    Removed troublesome backslash.

--- src/components/singlePages/About.js
+++ src/components/singlePages/About.js
@@ -8,7 +8,7 @@ export default props =>
     <p>
       Golbourn Training Solutions believes in making training fun, interactive and realistic. With our courses the dreaded mandatory training is a thing of the past. We will adapt our courses around each and every learner to maximise not only their individual experience but for the group as a whole. The right environment, the right atmosphere and the right instructor can get the best out of everyone. 
       
-      We make our First Aid Training scenarios as realistic as possible with the use of casualty simulation, this is not only fun and realistic, but it\'s a crucial learning tool. When your First Aider is in an emergency situation their training and muscle memory will make the difference.
+      We make our First Aid Training scenarios as realistic as possible with the use of casualty simulation, this is not only fun and realistic, but it is a crucial learning tool. When your First Aider is in an emergency situation their training and muscle memory will make the difference.
       
       Learning is developing, developing is investing, investing is securing success for the future. 
       
