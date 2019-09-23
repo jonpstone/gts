09679b007246e79b4a1b2b5f87771069b16507bd
Jonathan Stone <jpstone@protonmail.com>
2019-09-23 00:08:42 -0500

    Updated JSX tags in OrgSec.

--- src/components/singlePages/OrganisationSecurity.js
+++ src/components/singlePages/OrganisationSecurity.js
@@ -1,22 +1,29 @@
 import React from 'react';
+import { Container, Header} from 'semantic-ui-react'
 
-const OrgSec = () => (
-  <div className="orgSec">
-    <h2>Security Audit</h2>
-    <p>
-    	A review of your current security measures and processes and a report of our findings.
-    </p>
-    <h2>Lockdown Consultancy</h2>
-    <p>
-    	Consultancy and training to ensure your orgainisation is Lockdown ready.
-    </p>
-    <h2>Serious Incident Control Measures</h2>
-    <p>
-      As a Nationally registered CPD qualification, our Serious Incident Control Measures course 
-      will give the learner the training to identify security risks and manage security incidents.  
-      This course also includes serious injury first aid.
-    </p>
-  </div>
-);
+export default props =>
+  <React.Fragment>
+    <Container text style={{ marginTop: '7em' }}>
+      <Header as'h2'>Security Audit</h2>
+      <p>
+    	  A review of your current security measures and processes and a report of our findings.
+      </p>
+    </Container>
+    <Container text style={{ marginTop: '7em' }}>
+      <Header as'h2'>Lockdown Consultancy</h2>
+      <p>
+        Consultancy and training to ensure your orgainisation is Lockdown ready.
+      </p>
+      </Header>
+    </Container>
+    <Container text style={{ marginTop: '7em' }}>
+      <Header as'h2'>Serious Incident Control Measures</h2>
+      <p>
+        As a Nationally registered CPD qualification, our Serious Incident Control Measures course 
+        will give the learner the training to identify security risks and manage security incidents.  
+        This course also includes serious injury first aid.
+      </p>
+     </Container>
+  </React.Fragment>
 
 export default OrgSec;
