a66fd710d286edf8a9856cf9922664342627f70f
Jonathan Stone <jpstone@protonmail.com>
2019-09-29 18:20:24 -0500

    Updated text in Security Audit, including email tag.

--- src/components/orgSec/SecurityAudit.js
+++ src/components/orgSec/SecurityAudit.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import { Container } from 'semantic-ui-react'
 
-export default props => 
-  <Container text style={{ marginTop: '7em' }}>
-    Security Audit
-  </Container>
+export default props =>
+  <React.Fragment>
+    <Container text style={{ marginTop: '7em' }}>
+      Designed for organisations who have security processes and procedures in place and would like them tested to ensure they are 
+      effective.  Or for organisations who would like a review on what security measures are needed for there organisation.
+
+      Our team of security experts will visit your premises and will inspect and review your orgainisations security and provide 
+      a report of our findings.
+
+      For more information please <a href={"mailto:enqurires@golbourntrainingsolutions.co.uk"}>email us</a>.
+    </Container>
+  </React.Fragment>
+
