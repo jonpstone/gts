69e12da2a544c812dd4eb99106100ee1a842b837
Jonathan Stone <jpstone@protonmail.com>
2019-09-29 18:40:18 -0500

    Added text and email tag to Lockdown Consultancy.

--- src/components/orgSec/LockownConsultancy.js
+++ src/components/orgSec/LockownConsultancy.js
@@ -2,6 +2,64 @@ import React from 'react'
 import { Container } from 'semantic-ui-react'
 
 export default props => 
-  <Container text style={{ marginTop: '7em' }}>
-    Lockdown Consultancy
-  </Container>
+  <>
+    <Container text style={{ marginTop: '7em' }}>
+      <p>
+        In the current security climate across the United Kingdom and Europe, it is vital that identified potential targets are fully 
+        prepared to react effectively and instantly. Having a Lockdown procedure in place is essential, however your Lockdown is only as 
+        good as the people doing it.
+      </p>
+      <h4>Ask yourself these simple questions:</h4>
+
+      <ol>
+        <li>Do you have a Lockdown procedure?</li>
+        <li>Do your staff know and understand it?</li>
+        <li>Do they have the confidence and training to achieve it?</li>
+        <li>Are you confident it will work when it needs to?</li>
+        <li>Can your staff deal with a knife, gun or blast injuries?</li>
+        <li>Do you want our team to help?</li>  
+      </ol>
+      <p>
+        Our team of subject matter experts from across the UK military and Counterterrorist Police have 135 years combined operational 
+        experience in counterthreat and control measures implementation in both government and private sectors.
+        
+        See below the available options available from our team.
+        
+        <a href={"mailto:enquiries@golbourntrainingsolutions.co.uk}"}>Contact</a> us today to find the right option for you.
+      </p>
+    </Container>
+        
+    <Container text style={{ marginTop: '7em' }}>
+      
+      <h3>Basic Package</h3>
+      <p>
+        One of our instructors will meet with the Senior Leadership/Management Team to gain knowledge of current procedures and understand 
+        the layout.  They will then comprise a recommendation report, this will inform areas of improvement and recommendations.
+      </p>
+
+      <h3>Intermediate Package</h3>
+      <p>
+        One of our instructors will meet with the Senior Leadership/Management Team to gain knowledge of current procedures and understand 
+        the layout.  They will then design the bespoke procedures for your organisation within a week of this meeting.  This procedure will
+        then be shared with the Managemnt team and once approved the staff will then undergo training in the new procedures.
+      </p>
+
+      <h3>Intermediate Plus Package</h3>
+      <p>
+        One of our instructors will meet with the Senior Leadership/Management Team to gain knowledge of current procedures and understand 
+        the layout.  They will then design the bespoke procedures for your organistaion within a week of this meeting.  This procedure will 
+        then be shared with the Management team and once approved, the staff will then undergo training in the new procedures. All first 
+        aiders can undergo 3 hour upgrade CPD Regulated trauma training to deal with catastrophic injuries, ideal for first aid cover to 
+        Europe. HSE compliant qualification to buy and use frontline trauma first aid equipment like tourniquets, haemostatic products and 
+        chest seals.  
+      </p>
+
+      <h3>Advanced Package - Serious Incident Control Measures</h3>
+      <p>
+        The S.I.C.M Package can be suitable as a stand alone management system or as an intergrated cell of your current Corporate Incident 
+        Management system.
+      </p>
+
+      Click here
+     </Container>
+  </>
