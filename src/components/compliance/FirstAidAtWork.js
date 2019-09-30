eb8dc58139049ca2ea0d8afd458cd722ec3f5e4c
Jonathan Stone <jpstone@protonmail.com>
2019-09-29 19:35:49 -0500

    First Aid at Work Text Added.

--- src/components/compliance/FirstAidAtWork.js
+++ src/components/compliance/FirstAidAtWork.js
@@ -1,7 +1,70 @@
 import React from 'react'
 import { Container } from 'semantic-ui-react'
 
-export default props => 
-  <Container text style={{ marginTop: '7em' }}>
-    First Aid at Work
+export default props =>
+   <Container text style={{ marginTop: '7em' }}>
+     
+    <h2>First Aid at Work</h2>
+
+    <h3>Level 3 First Aid at Work: 3 day course</h3>
+
+    <p>
+      This comprehensive three-day course covers a wide range of first aid emergencies, enabling all participants to deal with emergency 
+      situations with confidence in a prompt, safe and effective way.
+    </p>
+    <p>
+      This course will give the participants a qualification to the highest level of First Aid, meeting the statutory requirements of the 
+      Health and Safety (First Aid) Regulations 1981.
+    </p>
+
+    <h4>Course Duration</h4>
+  
+    <p>
+      This course runs over 18 hours spread over at least 3 days. Ideally, the course should be run over three consecutive days, but we 
+      have the flexibility to accommodate your specific needs if you so desire providing it is completed within 10 weeks of starting the 
+      course and the training sessions last for a minimum of two hours.
+    </p>
+
+    <h4>Syllabus</h4>
+
+    A range of subjects are covered in:
+    
+    <ul>
+      <li>Legalities, responsibilities and reporting</li>
+      <li>Heart attacks</li>
+      <li>Dealing with an unresponsive casualty</li>
+      <li>Eye injuries</li>
+      <li>Fractures and spinal injuries</li>
+      <li>Choking</li>
+      <li>Control of bleeding</li>
+      <li>Chest injuries</li>
+      <li>Assessment of the situation</li>
+      <li>Diabetes</li>
+
+      <li>Shock (including Anaphylaxis)</li>
+      <li>Asthma</li>
+      <li>Head injuries</li>
+      <li>Poisoning</li>
+      <li>Sprains and strains</li>
+      <li>Stroke</li>
+      <li>Burns</li>
+      <li>Epilepsy</li>
+      <li>Resuscitation including AED</li>
+      <li>Bandaging</li>
+    </ul>
+
+    <p>
+      Each student will also receive a free comprehensive fully illustrated training book to supplement the First Aid at Work course this
+      will be provided in your training pack.
+    </p>
+    <p>
+      A maximum of 12 students are allowed and must be a minimum of 14 years of age and a certificate can be offered to all, subject to 
+      assessment.  A student cannot assume a responsibility in the workplace until they reach the age of 16, and then it is the employer's 
+      responsibility to ensure that the student is suitable for that role.
+    </p>
+    <p>
+      This qualification must be renewed every 3 years.  It is recommended that every first aider completes refresher training annually to 
+      keep their skills current and competent.
+    </p>
+
   </Container>
