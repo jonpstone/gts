e1f4a26eaadd61b50c6d2d83d9b9a426eb37835c
Jonathan Stone <jpstone@protonmail.com>
2019-10-14 16:37:25 -0500

    Filled out intitial text for Emergency First Aid.

--- src/components/compliance/EmergencyFirstAid.js
+++ src/components/compliance/EmergencyFirstAid.js
@@ -1,7 +1,75 @@
 import React from 'react'
 import { Container } from 'semantic-ui-react'
 
-export default props => 
-  <Container text style={{ marginTop: '7em' }}>
-    Emergency First Aid
-  </Container>
+export default props =>
+  <>
+    
+    <Container text style={{ marginTop: '7em' }}>
+      <Header as='h2'>Emergency First Aid at Work</Header>
+
+      <Header as='h3'>Level 3 Emergency First Aid at Work : 1 day course</Header>
+
+      <p>
+        The Emergency First Aid at Work course will enable your company to meet its 
+        regulatory requirements, if after completing a First Aid risk assessment your 
+        organisation is classed as a low risk working environment. Golbourn Training 
+        Solutions can provide First Aid risk assessment.
+      </p>
+
+      <p>
+        All learners will have the skills and knowledge to provide basic, lifesaving 
+        First Aid in the workplace.
+      </p>
+
+      <p>
+        <strong>Course Duration</strong><br />
+        This course runs over a minimum of six contact hours in the classroom, consisting
+        of practical and theoretical activities.
+      </p>
+
+      <p>
+        <strong>Syllabus</strong><br />
+        A range of subjects are covered including: -
+      </p>
+
+      <ul>
+        <li>Responsibilities and reporting</li>
+        <li>Assessment of the situation</li>
+        <li>Dealing with an unresponsive casualty</li>
+        <li>Basic hygiene in First Aid</li>
+        <li>Burns</li>
+        <li>Epilepsy</li>
+        <li>Resuscitation including AED</li>
+        <li>Anatomy</li>
+        <li>Minor injuries</li>
+        <li>Bleeding control</li>
+        <li>Choking</li>
+        <li>Shock</li>
+      </ul>
+
+      <p>
+        Please note add-ons like +F and Catastrophic Bleed Control can be run in 
+        conjunction, also this course includes AED training.
+      </p>
+
+      <p>
+        Each student will also receive a free comprehensive fully illustrated training 
+        book to supplement the Emergency First Aid at Work course this will be provided 
+        in your training pack.
+      </p>
+
+      <p>
+        A maximum of 12 students are allowed and must be a minimum of 14 years of age 
+        and a certificate can be offered to all, subject to assessment.  A student 
+        cannot assume a responsibility in the workplace until they reach the age of 16,
+        and then it is the employer's responsibility to ensure that the student is 
+        suitable for that role.
+      </p>
+
+      <p>
+        This qualification must be renewed every 3 years.  It is recommended that every
+        first aider completes refresher training annually to keep their skills current 
+        and competent.
+      </p>
+    </Container>
+  </>
