3356f487b6998e09031e81b078eebe902d2b560c
Jonathan Stone <jpstone@protonmail.com>
2019-10-14 17:14:02 -0500

    Added tags to Emergency Pediatric First Aid.

--- src/components/compliance/EmergencyPaediatricFirstAid.js
+++ src/components/compliance/EmergencyPaediatricFirstAid.js
@@ -5,32 +5,56 @@ export default props =>
   <Container text style={{ marginTop: '7em' }}>
     
     <Header as='h2'>Emergency Paediatric First Aid</Header> 
+    <p>
+      This one-day Emergency Paediatric First Aid course has been designed for those 
+      who have an interest in child and infant basic life support.
+    </p>
 
-    This one-day Emergency Paediatric First Aid course has been designed for those who have an interest in child and infant basic life support.
+    <p>
+      If you have a professional interest, i.e. you are a child-minder or school 
+      teacher, etc, then this course partly fulfils the requirements of Ofsted and 
+      the Early Years Foundation Stage (EYFS). Ofsted and the Early Years Foundation 
+      Stage require you to have 12 hours of Paediatric First Aid Training, so the 
+      2-Day Level 3 Award in Paediatric First Aid would be required to fulfil their 
+      requirements. This 6 hour qualification also makes up the first half of the 
+      12-hour course.
+    </p>
 
-    If you have a professional interest, i.e. you are a child-minder or school teacher, etc, then this course partly fulfils the requirements of Ofsted and the Early Years Foundation Stage (EYFS). Ofsted and the Early Years Foundation Stage require you to have 12 hours of Paediatric First Aid Training, so the 2-Day Level 3 Award in Paediatric First Aid would be required to fulfil their requirements. This 6 hour qualification also makes up the first half of the 12-hour course.
+    <p>
+      <strong>Course Duration</strong>
+      A minimum of six hours makes up this qualification consisting of theoretical and 
+      practical work.
+    </p>
 
-    Course Duration
+    <p>
+      <strong>Syllabus</strong>
+      A range of subjects are covered including:
+    </p>
 
-    A minimum of six hours makes up this qualification consisting of theoretical and practical work.
+    <li>Responsibilities and reporting</li>
+    <li>Resuscitation including AED</li>
+    <li>Dealing with an unresponsive casualty</li>
+    <li>Minor injuries </li>
+    <li>Assessment of the situation</li>
+    <li>Epilepsy</li>
+    <li>Wounds and bleeding</li>
+    <li>Anaphylaxis and Shock</li>
 
-    Syllabus
+    <p>
+      Each student will also receive a free comprehensive fully illustrated training 
+      book to supplement the Emergency Paediatric First Aid course this will be 
+      provided in your training pack.
+    </p>
 
-    A range of subjects are covered including:
+    <p>
+      A maximum of 12 students are allowed and must be a minimum of 16 years of age 
+      and a certificate can be offered to all, subject to assessment. It is the 
+      employer's responsibility to ensure that the student is suitable for that role.
+    </p>
 
-    Responsibilities and reporting
-    Resuscitation including AED
-    Dealing with an unresponsive casualty
-    Minor injuries 
-
-    Assessment of the situation
-    Epilepsy
-    Wounds and bleeding
-    Anaphylaxis and Shock
-
-    Each student will also receive a free comprehensive fully illustrated training book to supplement the Emergency Paediatric First Aid course this will be provided in your training pack.
-
-    A maximum of 12 students are allowed and must be a minimum of 16 years of age and a certificate can be offered to all, subject to assessment.  It is the employer's responsibility to ensure that the student is suitable for that role.
-
-    This qualification must be renewed every 3 years.  It is recommended that every first aider completes refresher training annually to keep their skills current and competent.
+    <p>
+      This qualification must be renewed every 3 years. It is recommended that every 
+      first aider completes refresher training annually to keep their skills current 
+      and competent.
+    </p>
   </Container>
