eb8bc41a43beb41e971e86a7435c4e609df9d789
Jonathan Stone <jpstone@protonmail.com>
2019-10-21 21:52:15 -0500

    Initial text and tags for Activity First Aid.

--- src/components/compliance/ActivityFirstAid.js
+++ src/components/compliance/ActivityFirstAid.js
@@ -1,7 +1,75 @@
 import React from 'react'
-import { Container } from 'semantic-ui-react'
+import { Container, Header } from 'semantic-ui-react'
 
 export default props => 
-  <Container text style={{ marginTop: '7em' }}>
-    Activity First Aid
-  </Container>
+  <>
+    <Container text style={{ marginTop: '7em' }}>
+      <Header as='h2'>Activity First Aid</Header>
+
+      <p>
+        This Level 3 qualification is made up of two units including First Aid Essentials. 
+        It is specifically designed for those who are involved in any form of activity, 
+        including sports, leisure and recreation.
+      </p>
+
+      <p>
+        Not only is Basic Life Support included, but many other First Aid conditions are 
+        also covered, making the full qualification very comprehensive.
+      </p>
+      
+      <p>
+        <strong>Course Duration</strong>
+        A minimum of 12 contact hours in the classroom, consisting of practical and 
+        theoretical activities.
+      </p>
+      
+      <p>
+        <strong>Syllabus</strong>
+        A range of subjects are covered including:
+      </p>
+
+      <ul>
+        <li>Responsibilities and reporting</li>
+        <li>Heart attack</li>
+        <li>ssessment of the situation</li>
+        <li>Resuscitation including AED</li>
+        <li>Dealing with an unresponsive casualty</li>
+        <li>Minor injuries</li>
+        <li>Basic hygiene in First Aid</li>
+        <li>Bleeding control</li>
+        <li>Fractures</li>
+        <li>Head injuries</li>
+        <li>Drowning injuries</li>
+        <li>Diabetes</li>
+        <li>Electrical injuries</li>
+        <li>Angina</li>
+        <li>Spinal injuries</li>
+        <li>Choking</li>
+        <li>Heat and cold</li>
+        <li>Seizures</li>
+        <li>Burns and Shock</li>
+        <li>Asthma</li>
+      </ul>
+
+      <p>
+        Each student will also receive a free comprehensive fully illustrated training
+        book to supplement the Activity First Aid course this will be provided in your 
+        training pack.
+      </p>
+
+      <p>
+        A maximum of 12 students are allowed and must be a minimum of 14 years of age 
+        and a certificate can be offered to all, subject to assessment. A student 
+        cannot assume a responsibility in the workplace until they reach the age of 16, 
+        and then it is the employer’s responsibility to ensure that the student is 
+        suitable for that role. The certificate will expire as dated on the certificate, 
+        irrespective of age.
+      </p>
+
+      <p>
+        This qualification must be renewed every 3 years.  It is recommended that every
+        first aider completes refresher training annually to keep their skills current 
+        and competent.
+      </p>
+    </Container>
+  </>
