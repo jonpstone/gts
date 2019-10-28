f0b2113526c45e11138ecdb6b2528a03c7f4c053
Jonathan Stone <jpstone@protonmail.com>
2019-10-27 23:34:42 -0500

    Added text to AnaphylaxisManagement.

--- src/components/compliance/AnaphylaxisManagement.js
+++ src/components/compliance/AnaphylaxisManagement.js
@@ -3,5 +3,50 @@ import { Container, Header, Image} from 'semantic-ui-react'
 
 export default props => 
   <Container text style={{ marginTop: '7em' }}>
-    Anaphylaxis management
+    An Anaphylactic shock, or anaphylaxis, is a life-threatening, serious allergic reaction to an allergen such as food substances or insect stings. If it is not treated appropriately, it may cause death. This training is for qualified First Aiders and Emergency First Aiders to enable them to recognise and treat anaphylaxis safely and effectively.
+
+    When administering a medication, it is important to recognise and differentiate between allergic reactions and the potentially life-threatening anaphylactic reactions immediately. It is also essential to determine whether the individual has had an allergic or anaphylactic response to the medication in the past or prior to administration.
+
+    This course is suitable for all First Aiders and healthcare professionals, as well as those in a high risk environment where such medication is available.
+
+    Course Duration
+
+    This is a classroom-based course of 6 contact hours.
+
+    Syllabus
+
+    The course is made up of practical and theoretical sessions delivered by your Instructor who will provide a detailed course programme.
+
+    At the end of the course, learners will be able to:
+
+        Describe anaphylaxis
+        Identify triggers for anaphylaxis
+        Describe life threatening problems associated with the:
+        Airway
+        Breathing
+        Circulation
+        Disability (Neurological status)
+        Exposure (Skin and mucosal changes)
+        Explain the need for an early call for help
+
+    Explain the treatment for anaphylaxis:
+
+        Non re-breather mask
+        Removal of trigger
+        Auto-injector
+        Identify key features of adrenaline-auto injectors
+        Conduct an initial assessment using the ABCDE approach
+        Demonstrate a recovery position
+        Demonstrate resuscitation for adult and/or child
+        Recognise the need to use an auto-injector
+        Prepare medication for administration during anaphylaxis treatment
+        Demonstrate the safe use of an adrenaline auto-injector using a training device
+        Safely dispose of sharps according to agreed ways of working
+        Handover the casualty to a medical professional
+
+    Each student will also receive a free comprehensive fully illustrated training book to supplement the Anaphylaxis Management course this will be provided in your training pack.
+
+    A maximum of 12 students are allowed and must be a minimum of 16 years of age.
+
+    This qualification lasts for 3 years. To renew their qualification after 3 years, delegates will need to retake the full course. However, it is recommended that, during the 3 year period, delegates attend annual refresher courses.
   </Container>
