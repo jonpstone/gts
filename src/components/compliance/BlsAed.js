8e3e9d829dafa764c8721c4465bd017e3a31fddb
Jonathan Stone <jpstone@protonmail.com>
2019-10-27 23:30:56 -0500

    Added text to blsaed.

--- src/components/compliance/BlsAed.js
+++ src/components/compliance/BlsAed.js
@@ -3,5 +3,38 @@ import { Container } from 'semantic-ui-react'
 
 export default props => 
   <Container text style={{ marginTop: '7em' }}>
-    BLS AED
+    Heart and circulatory disease claims around 200,000 lives each year. Many could have been saved if a defibrillator was made available.
+
+    Automated External Defibrillators (AED’s) are now becoming more common throughout a range of public and work places. A casualty’s survival rate will dramatically increase if an AED is available and if you have appropriately qualified personnel to use it.
+
+    This very practical course will enable your learners to use the AED competently and with confidence.
+
+    Not only does this course deal with the use of an AED but it also includes vital training in resuscitation and choking procedures.
+
+    Course Duration
+
+    A minimum of six contact hours in the classroom, consisting of practical and theoretical activities.
+
+    Syllabus
+
+    A range of subjects are covered including:
+
+        Responsibilities and reporting
+        Dealing with an unresponsive casualty
+        Assessment of the situation
+        Paediatric defibrillation
+        Defibrillation procedures
+        Resuscitation
+        Choking
+        Heart attacks
+        AED safety and Functions
+        Infection control 
+
+    CPR and AED training is fast becoming a necessity. Golbourn Training Solutions not only offer this dedicated CPR and AED training course but also a 4 hour Basic Life Support and AED course.
+
+    Each student will also receive a free comprehensive fully illustrated training book to supplement the CPR and AED course this will be provided in your training pack. 
+
+    A maximum of 12 students are allowed and must be a minimum of 16 years of age.
+
+    This qualification lasts for 3 years. To renew their qualification after 3 years, delegates will need to retake the full course. However, it is recommended that, during the 3 year period, delegates attend annual refresher courses.
   </Container>
