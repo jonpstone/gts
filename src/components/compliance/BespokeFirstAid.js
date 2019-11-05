279b1f52bd643d8b68feaf58b66079da852ab27e
Jonathan Stone <jpstone@protonmail.com>
2019-11-04 22:08:58 -0600

    Initial markup for bespokeFirstAid.

--- src/components/compliance/BespokeFirstAid.js
+++ src/components/compliance/BespokeFirstAid.js
@@ -3,5 +3,13 @@ import { Container } from 'semantic-ui-react'
 
 export default props => 
   <Container text style={{ marginTop: '7em' }}>
-    He's a big monkey man
+    Golbourn Training Solutions will develop a First Aid Course to meet your organizations specific needs, we have run courses for Scout Groups, Cadet Forces, Sporting Groups and many more.
+
+    If you have a set program or syllabus, we will base our training model to meet its specific needs.
+
+    Alternately we can draw up a course based on what you want to achieve.
+
+    Practical and theory based assessments, with scenario exercises can be used to great effect.
+
+    Please Contact us to arrange a course just for you.
   </Container>
