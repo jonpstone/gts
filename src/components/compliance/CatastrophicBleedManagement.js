b9731007ef5e35595db1d2390fc6af5f550aa16e
Jonathan Stone <jpstone@protonmail.com>
2019-11-04 22:15:09 -0600

    Initial text for CatastrophicBleeds.

--- src/components/compliance/CatastrophicBleedManagement.js
+++ src/components/compliance/CatastrophicBleedManagement.js
@@ -3,5 +3,25 @@ import { Container } from 'semantic-ui-react'
 
 export default props => 
   <Container text style={{ marginTop: '7em' }}>
-    Catastrophic bleed management
+    Our Golbourn Training Solutions Catastrophic Bleed Management Course is ideal for identified high risk industries such as farming, stalking, gun clubs, security, construction, forestry, tree surgery and many more.
+
+    It can be run in the following options:
+
+    As a 2-3 hour stand alone course.
+
+    As a bolt on to a regulated course, such as EFAW or FAW.
+
+    As an add-on to the +F bolt on course.
+
+    As part of the bespoke serious incident control measures course.
+
+    Course Content:
+
+    Changes to UK Guidelines.
+    Recognising a Catastrophic External Bleed.
+    Equipment use and maintenance - Pressure Bandages, Tourniquets and multiple CELOX      products.
+    Catastrophic External Bleeding Control Measures Methods.
+    Casualty After Care.
+    Dangers of the incorrect usage of Tourniquets and Haemostatic products.
+    Practical training in realistic scenario tests, to suit each client’s environment.
   </Container>
