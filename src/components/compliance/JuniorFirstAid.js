a1e81e6fada4cd9295d63c343ae5669e68c0e0a0
Jonathan Stone <jpstone@protonmail.com>
2019-11-04 22:12:12 -0600

    Markup JuniorFirstAid.

--- src/components/compliance/JuniorFirstAid.js
+++ src/components/compliance/JuniorFirstAid.js
@@ -3,5 +3,21 @@ import { Container } from 'semantic-ui-react'
 
 export default props => 
   <Container text style={{ marginTop: '7em' }}>
-    Junior First Aid
+    At Golbourn Training Solutions we believe that children can save lives too. That is why we have designed our Junior First Aiders Course.  Aimed at teaching children as young as 8 how to save lives.
+    
+      Syllabus
+
+      Contents of the First Aid Box
+      Emergency first aid numbers and information needed
+      What to do if you find a poorly person
+      CPR
+      Recovery position
+      Heart attack/Stroke
+      Asthma
+      Choking
+      Burns
+      Epilepsy
+      Bleeding
+
+    This course can be taught in a whole day or broken down to four 2 hour lessons.  Essentially we can work with you to make the course fit around your other needs.
   </Container>
