dccaf1cfeb459029be0be5d8bb2a37f3a8540ae1
Jonathan Stone <jpstone@protonmail.com>
2019-11-11 16:54:18 -0600

    Added tags for CatastropicBleeds.

--- src/components/compliance/CatastrophicBleedManagement.js
+++ src/components/compliance/CatastrophicBleedManagement.js
@@ -3,25 +3,28 @@ import { Container } from 'semantic-ui-react'
 
 export default props => 
   <Container text style={{ marginTop: '7em' }}>
-    Our Golbourn Training Solutions Catastrophic Bleed Management Course is ideal for identified high risk industries such as farming, stalking, gun clubs, security, construction, forestry, tree surgery and many more.
+    <p>
+      Our Golbourn Training Solutions Catastrophic Bleed Management Course is ideal for 
+      identified high risk industries such as farming, stalking, gun clubs, security, 
+      construction, forestry, tree surgery and many more.
+    </p>
 
-    It can be run in the following options:
+    <strong>It can be run in the following options:</strong>
+    <ul>
+      <li>As a 2-3 hour stand alone course.</li>
+      <li>As a bolt on to a regulated course, such as EFAW or FAW.</li>
+      <li>As an add-on to the +F bolt on course.</li>
+      <li>As part of the bespoke serious incident control measures course.</li>
+    </ul>
 
-    As a 2-3 hour stand alone course.
-
-    As a bolt on to a regulated course, such as EFAW or FAW.
-
-    As an add-on to the +F bolt on course.
-
-    As part of the bespoke serious incident control measures course.
-
-    Course Content:
-
-    Changes to UK Guidelines.
-    Recognising a Catastrophic External Bleed.
-    Equipment use and maintenance - Pressure Bandages, Tourniquets and multiple CELOX      products.
-    Catastrophic External Bleeding Control Measures Methods.
-    Casualty After Care.
-    Dangers of the incorrect usage of Tourniquets and Haemostatic products.
-    Practical training in realistic scenario tests, to suit each client’s environment.
+    <strong>Course Content:</strong>
+    <ul>
+      <li>Changes to UK Guidelines.</li>
+      <li>Recognising a Catastrophic External Bleed.</li>
+      <li>Equipment use and maintenance - Pressure Bandages, Tourniquets and multiple CELOX products.</li>
+      <li>Catastrophic External Bleeding Control Measures Methods.</li>
+      <li>Casualty After Care.</li>
+      <li>Dangers of the incorrect usage of Tourniquets and Haemostatic products.</li>
+      <li>Practical training in realistic scenario tests, to suit each client’s environment.</li>
+    </ul>
   </Container>
