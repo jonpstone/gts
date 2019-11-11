3e53328c3b2671f9646b092fefeaef97d9408996
Jonathan Stone <jpstone@protonmail.com>
2019-11-11 13:51:09 -0600

    Added HTML tags to component text for CprBlsAed.

--- src/components/compliance/CprBlsAed.js
+++ src/components/compliance/CprBlsAed.js
@@ -1,7 +1,75 @@
 import React from 'react'
-import { Container } from 'semantic-ui-react'
+import { Container, Header } from 'semantic-ui-react'
 
 export default props => 
   <Container text style={{ marginTop: '7em' }}>
-    CPR, BLS & AED
+    <Header as='h2'>CPR/BLS and AED</Header>
+
+    <p>
+      Heart and circulatory disease claims around 200,000 lives each year. Many could 
+      have been saved if a defibrillator was made available.
+    </p>
+
+    <p>
+      Automated External Defibrillators (AED’s) are now becoming more common throughout 
+      a range of public and work places. A casualty’s survival rate will dramatically 
+      increase if an AED is available and if you have appropriately qualified personnel 
+      to use it.
+    </p>
+
+    <p>
+      This very practical course will enable your learners to use the AED competently 
+      and with confidence.
+    </p>
+
+    <p>
+      Not only does this course deal with the use of an AED but it also includes vital 
+      training in resuscitation and choking procedures.
+    </p>
+
+    <p>
+      <strong>Course Duration</strong><br />
+      A minimum of six contact hours in the classroom, consisting of practical and 
+      theoretical activities.
+    </p>
+    
+    <p>
+      <strong>Syllabus</strong><br />
+      A range of subjects are covered including:
+    </p>
+
+    <ul>
+      <li>Responsibilities and reporting</li> 
+      <li>Dealing with an unresponsive casualty</li>
+      <li>Assessment of the situation</li>
+      <li>Paediatric defibrillation</li>
+      <li>Defibrillation procedures</li>
+      <li>Resuscitation</li>
+      <li>Choking</li>
+      <li>Heart attacks</li>
+      <li>AED safety and Functions</li>
+      <li>Infection control</li>
+    </ul>
+
+    <p>
+      CPR and AED training is fast becoming a necessity. Golbourn Training Solutions 
+      not only offer this dedicated CPR and AED training course but also a 4 hour 
+      Basic Life Support and AED course.
+    </p>
+
+    <p>
+      Each student will also receive a free comprehensive fully illustrated training 
+      book to supplement the CPR and AED course this will be provided in your training
+      pack.
+    </p>
+
+    <p>
+      A maximum of 12 students are allowed and must be a minimum of 16 years of age.
+    </p>
+    
+    <p>
+      This qualification lasts for 3 years. To renew their qualification after 3 
+      years, delegates will need to retake the full course. However, it is recommended
+      that, during the 3 year period, delegates attend annual refresher courses.
+    </p>
   </Container>
