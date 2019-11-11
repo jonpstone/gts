a5c280cb90fcfe819d2f3c90eff9e088caede5d5
Jonathan Stone <jpstone@protonmail.com>
2019-11-11 16:07:59 -0600

    Added tags for AnaphylaxisManagement.

--- src/components/compliance/AnaphylaxisManagement.js
+++ src/components/compliance/AnaphylaxisManagement.js
@@ -2,51 +2,81 @@ import React from 'react'
 import { Container, Header, Image} from 'semantic-ui-react'
 
 export default props => 
-  <Container text style={{ marginTop: '7em' }}>
-    An Anaphylactic shock, or anaphylaxis, is a life-threatening, serious allergic reaction to an allergen such as food substances or insect stings. If it is not treated appropriately, it may cause death. This training is for qualified First Aiders and Emergency First Aiders to enable them to recognise and treat anaphylaxis safely and effectively.
+  <Container text style={{ marginTop: '7em' }}>    
+    <Header as='h2'>Anaphylaxis Management</Header>
 
-    When administering a medication, it is important to recognise and differentiate between allergic reactions and the potentially life-threatening anaphylactic reactions immediately. It is also essential to determine whether the individual has had an allergic or anaphylactic response to the medication in the past or prior to administration.
+    <p> 
+      An anaphylactic shock, or anaphylaxis, is a life-threatening, serious allergic reaction to an allergen such as food substances or insect stings. If it is not treated appropriately, it may cause death. This training is for qualified First Aiders and Emergency First Aiders to enable them to recognise and treat anaphylaxis safely and effectively.
+    </p>
+    
+    <p>
+      When administering a medication, it is important to recognise and differentiate between allergic reactions and the potentially life-threatening anaphylactic reactions immediately. It is also essential to determine whether the individual has had an allergic or anaphylactic response to the medication in the past or prior to administration.
+    </p>
 
-    This course is suitable for all First Aiders and healthcare professionals, as well as those in a high risk environment where such medication is available.
+    <p>
+      This course is suitable for all First Aiders and healthcare professionals, as well as those in a high risk environment where such medication is available.
+    </p>
 
-    Course Duration
+    <p>
+      <strong>Course Duration</strong><br />
+      This is a classroom-based course of 6 contact hours.
+    </p>
 
-    This is a classroom-based course of 6 contact hours.
+    <p>
+      <strong>Syllabus</strong><br />
+      The course is made up of practical and theoretical sessions delivered by your 
+      instructor who will provide a detailed course programme.
+    </p>
 
-    Syllabus
+    <p>
+      At the end of the course, learners will be able to:
+    </p>
 
-    The course is made up of practical and theoretical sessions delivered by your Instructor who will provide a detailed course programme.
+    <ul>
+        <li>Describe anaphylaxis</li>
+        <li>Identify triggers for anaphylaxis</li>
+        <li>Describe life threatening problems associated with the:
+          <ul>
+            <li>Airway</li>
+            <li>Breathing</li>
+            <li>Circulation</li>
+            <li>Disability (Neurological status)</li>
+            <li>Exposure (Skin and mucosal changes)</li>
+          </ul>
+        </li>
+      <li>Explain the need for an early call for help</li>
+      <li>Explain the treatment for anaphylaxis:
+        <ul>
+          <li>Non re-breather mask</li>
+          <li>Removal of trigger</li>
+          <li>Auto-injector</li>
+          <li>Identify key features of adrenaline-auto injectors</li>
+          <li>Conduct an initial assessment using the ABCDE approach</li>
+          <li>Demonstrate a recovery position</li>
+          <li>Demonstrate resuscitation for adult and/or child</li>
+          <li>Recognise the need to use an auto-injector</li>
+          <li>Prepare medication for administration during anaphylaxis treatment</li>
+          <li>Demonstrate the safe use of an adrenaline auto-injector using a training device</li>
+          <li>Safely dispose of sharps according to agreed ways of working</li>
+          <li>Handover the casualty to a medical professional</li>
+        </ul>
+      </li>
+    </ul>
 
-    At the end of the course, learners will be able to:
+    <p>
+      Each student will also receive a free comprehensive fully illustrated training 
+      book to supplement the Anaphylaxis Management course this will be provided in 
+      your training pack.
+    </p>
 
-        Describe anaphylaxis
-        Identify triggers for anaphylaxis
-        Describe life threatening problems associated with the:
-        Airway
-        Breathing
-        Circulation
-        Disability (Neurological status)
-        Exposure (Skin and mucosal changes)
-        Explain the need for an early call for help
-
-    Explain the treatment for anaphylaxis:
-
-        Non re-breather mask
-        Removal of trigger
-        Auto-injector
-        Identify key features of adrenaline-auto injectors
-        Conduct an initial assessment using the ABCDE approach
-        Demonstrate a recovery position
-        Demonstrate resuscitation for adult and/or child
-        Recognise the need to use an auto-injector
-        Prepare medication for administration during anaphylaxis treatment
-        Demonstrate the safe use of an adrenaline auto-injector using a training device
-        Safely dispose of sharps according to agreed ways of working
-        Handover the casualty to a medical professional
-
-    Each student will also receive a free comprehensive fully illustrated training book to supplement the Anaphylaxis Management course this will be provided in your training pack.
-
-    A maximum of 12 students are allowed and must be a minimum of 16 years of age.
-
-    This qualification lasts for 3 years. To renew their qualification after 3 years, delegates will need to retake the full course. However, it is recommended that, during the 3 year period, delegates attend annual refresher courses.
-  </Container>
+    <p>
+      A maximum of 12 students are allowed and must be a minimum of 16 years of age.
+    </p>  
+    
+    <p>
+      This qualification lasts for 3 years. To renew their qualification after 3 
+      years, delegates will need to retake the full course. However, it is 
+      recommended that, during the 3 year period, delegates attend annual refresher 
+      courses.
+    </p>   
+ </Container>
