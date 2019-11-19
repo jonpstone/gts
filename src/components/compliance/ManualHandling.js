436b46412476115982f43c325ed67764e5f989a6
Jonathan Stone <jpstone@protonmail.com>
2019-11-18 22:27:26 -0600

    JSX for manual handling.

--- src/components/compliance/ManualHandling.js
+++ src/components/compliance/ManualHandling.js
@@ -3,5 +3,35 @@ import { Container } from 'semantic-ui-react'
 
 export default props => 
   <Container text style={{ marginTop: '7em' }}>
-    Manual Handling
+    
+Manual Handling
+Level 2 Manual Handling Course (Principles and Practice) : 1 day
+
+ 
+
+We are all exposed at times to some form of manual handling such as lifting, carrying, pulling and pushing actions that can be compounded by simultaneous movements such as climbing or pivoting. Personal injuries resulting from incorrect manual handling in the workplace accounts for over one million working days lost per year, as well as the pain and suffering experienced by the casualties. Correct manual handling techniques can make a huge difference.
+
+ 
+
+The risks arising from manual handling have been recognised by the Health and Safety Executive for many years, as evidenced in their regulations and advisory activities promoting good practice.
+
+ 
+
+The Regulations require employers to provide their employees with health and safety information and training – supplemented, as necessary, with more specific information and training on manual handling injury risks and prevention.
+
+ 
+
+This course provides an ideal approach to safer and more effective manual handling – not only meeting HSE recommendations, but also promoting good and safe practices in any situation.
+
+ 
+
+Syllabus
+
+A range of subjects are covered that will equip delegates to:
+
+    Understand the reasons for safe manual handling
+    Understand how manual handling risk assessments contribute to improving health and safety
+    Understand the principles, types of equipment and testing requirements associated with manual handling safety
+    Be able to apply safe manual handling principles
+
   </Container>
