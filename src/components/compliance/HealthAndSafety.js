6e167bf94e8fbc627e028a21d861ea67514244f1
Jonathan Stone <jpstone@protonmail.com>
2019-11-18 22:25:15 -0600

    Initial JSX for Health and Safety.

--- src/components/compliance/HealthAndSafety.js
+++ src/components/compliance/HealthAndSafety.js
@@ -3,5 +3,66 @@ import { Container } from 'semantic-ui-react'
 
 export default props => 
   <Container text style={{ marginTop: '7em' }}>
-    Health and Safety
+    
+Health & Safety Training
+
+ 
+
+All workers have a right to work in places where risks to their health and safety are properly controlled. Health and safety is about stopping you getting hurt at work or ill through work. 
+
+ 
+
+It is the employer's responsibility to:
+
+    Carryout Risk Assessments.
+    Put in place control measures.
+    Provide information.
+    Provide adequate training.
+    Provide appropriate level of supervision.
+
+Level 2 Health & Safety in the workplace: 1-day course
+
+This is an excellent introduction to health & safety in the workplace and will benefit all employees, particularly those who may be accepting a health and safety role, such as a safety represetative within their organisation.  Office/site manager etc.
+
+ 
+
+Successful candidates will be awarded a national Level 2 (Level 5 in Scotland) qualification which will provide an ideal platform to higher level training or career development.
+
+ 
+
+Syllabus
+
+ 
+
+A range of subjects are covered on this course including:
+
+    The roles and responsibilities for health, safety and welfare in the workplace
+    The va;ue and process of risk assessments
+    The identification and control of workplace hazards
+    How to respond to workplace incidents and accidents.
+
+Level 1 Health & Safety in the workplace : 4 hour course
+
+This is an excellent introduction to health and safety in the workplace and will benefit any worker, in any industry or sector and accommodates induction training for the new employee very well.
+
+ 
+
+All aspects of Health and Safety are important to a business whether you are an employee, contractor or supplier and this half-day course will benefit both the learner and the business, ensuring that the working environment is a safe and healthy one.
+
+ 
+
+Syllabus
+
+ 
+
+A range of subjects are covered including:
+
+    Understanding the importance of health, safety and welfare standards in the workplace
+    Knowing how hazards and risks are controlled in the workplace
+    Being aware of the main causes and effects of poor health and safety at work
+    Understanding how health and safety information is communicated in the workplace.
+
+ 
+
+We can also design bespoke non regulatated health and safety courses designed specifically for your individual needs.
   </Container>
