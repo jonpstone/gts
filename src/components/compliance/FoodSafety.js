88cc193aafeae750b5f9fa552b410bfde786e4f8
Jonathan Stone <jpstone@protonmail.com>
2019-11-18 22:31:52 -0600

    JSX for Food Safety.

--- src/components/compliance/FoodSafety.js
+++ src/components/compliance/FoodSafety.js
@@ -3,5 +3,85 @@ import { Container } from 'semantic-ui-react'
 
 export default props => 
   <Container text style={{ marginTop: '7em' }}>
-    Food Safety
+    
+
+Level 2 Food Safety in Catering : 1-day course
+
+ 
+
+This is an excellent introduction to Food Safety in Catering and will benefit all employees at all levels, particularly those who hold, or are about to hold, a food safety role within their catering organisation.
+
+ 
+
+The course will clarify aspects of food safety which many of us believe to be complex and daunting. The importance of food safety in catering is underpinned by health and safety legislation that places a duty on employers to establish policies and procedures and ensure their employees receive appropriate training.
+
+ 
+
+Successful candidates will be awarded a national Level 2 (Level 5 in Scotland) qualification which will provide an ideal platform to higher level training or career development.
+
+ 
+
+Syllabus
+
+A range of subjects are covered including:
+
+ 
+
+    How individuals can take responsibility for food safety
+    The importance of keeping him/herself clean and hygienic
+    The importance of keeping the work areas clean and hygienic
+    The importance of keeping food safe
+
+Level 2 Good Nutrition : Half-day course
+
+ 
+
+This 4 hour Good Nutrition course is designed for anyone who is working or seeking a career in food preparation and/or food service. It will give you basic knowledge of nutrition which will help you prepare well-balanced meals and menus, the result of which will have a positive impact on health as well as an understanding of special dietary requirements.
+
+ 
+
+If you particularly work in hospital or school kitchens, residential homes or community based kitchens, then this course will assist you in preparing well balanced and nutritional meals.
+
+ 
+
+If you are considering a career in the food industry, then this course will give you a good understanding and appreciation of nutritional values and well-balanced diets.
+
+ 
+
+It will also help those who are active in sport and recreation, children or adults, in respect of producing well-balanced diets to support that activity.
+
+ 
+
+Syllabus
+
+The course is made up of theoretical sessions delivered by your Instructor who will provide a detailed course programme.
+
+ 
+
+A range of subjects are covered including:
+
+ 
+
+    The principle of balanced diets
+    Current Government nutritional guidelines for a healthy diet
+    Sources of essential nutrients
+    The impact of diet on health
+    Catering practices that help maintain the nutritional value of food
+    How to plan and provide special diets
+    The impact of special diets on health
+    Catering practices to be considered when planning and providing meals for those on special diets
+
+ 
+
+Certification
+
+This is achieved by on-going theoretical teaching by the instructor, culminating with a written test.
+
+This qualification does not expire, but refresher training and keeping up to date with changes to policies and procedures is vital to keep the qualification current.
+
+ 
+
+Numbers
+
+A maximum of 16 students are allowed and must be a minimum of 14 years of age and a certificate can be offered to all, subject to assessment.
   </Container>
