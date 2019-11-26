98e4eed46a0d9f475f50d1cd5fcbff2314fe0a7d
Jonathan Stone <jpstone@protonmail.com>
2019-11-25 21:11:51 -0600

    HTML tags for Mental First Ai.

--- src/components/compliance/MentalHealthFirstAid.js
+++ src/components/compliance/MentalHealthFirstAid.js
@@ -1,78 +1,135 @@
 import React from 'react'
-import { Container } from 'semantic-ui-react'
+import { Container, Header } from 'semantic-ui-react'
 
 export default props => 
   <Container text style={{ marginTop: '7em' }}>
+    <Header as='h2'>Mental Health First Aid</Header>
     
-    There are three levels of Mental Health First Aid course each designed with different levels of responsibility in mind.
+    <p>
+      There are three levels of Mental Health First Aid course each designed with 
+      different levels of responsibility in mind.
+    </p>
 
-    Level 1 Award in Awareness of First Aid for Mental Health – ½ Day
+    <strong>Level 1 Award in Awareness of First Aid for Mental Health – ½ Day</strong>
+    <p>
+      Anyone can be affected by a mental health condition either themselves or a family 
+      member, friend or colleague.
+    </p>
 
-    Anyone can be affected by a mental health condition either themselves or a family member, friend or colleague.
+    <p>
+      This awareness course is suitable for everyone as it provides learners with the 
+      knowledge to recognise a suspected mental health condition and the skills to start
+      a conversation and be able to signpost a person towards professional help.
+    </p>
 
-    This awareness course is suitable for everyone as it provides learners with the knowledge to recognise a suspected mental health condition and the skills to start a conversation and be able to signpost a person towards professional help.
+    <p>
+      All members of the public can benefit from this course and it is a great stepping 
+      stone into First Aid for Mental Health.
+    </p>
 
-    All members of the public can benefit from this course and it is a great stepping stone into First Aid for Mental Health.
+    <strong>Syllabus</strong>
+    <p>
+      A range of subjects are covered including:
+    </p>
 
-    Syllabus
-    A range of subjects are covered including:
+    <ul>
+      <li>What is First Aid for Mental Health?</li>       
+      <li>Identifying mental health conditions</li>        
+      <li>Providing advice and starting a conversation</li>      
+    </ul>
 
-    What is First Aid for Mental Health?
+    <strong>Level 2 Award in First Aid for Mental Health – 1 Day</strong>
+    <p>
+      This course is suitable for everyone but has been designed to help employers to 
+      provide a positive mental health culture within the workplace and to provide 
+      learners with comprehensive knowledge on a range of the most common mental health 
+      conditions and the skills to be able to act should a condition be suspected.
+    </p>
 
-    Identifying mental health conditions
+    <p>
+      Learners undertaking this course will be considered First Aiders for Mental Health 
+      and be a point of contact within the workplace to help and support those with a 
+      suspected mental health condition.
+    </p>
 
-    Providing advice and starting a conversation
+    <p>
+      Building on the Level 1 Award in Awareness of First Aid for Mental Health this 
+      course introduces the First Aid for Mental Health Action Plan which allows 
+      learners to provide their peers with comprehensive guidance and support for a 
+      suspected mental health condition. Learners will be able to provide signposting 
+      to professional help and support that person whilst they are receiving professional 
+      assistance.
+    </p>
 
-    Level 2 Award in First Aid for Mental Health – 1 Day
+    <p>
+      This course also covers the effects of drugs and alcohol and provides learners 
+      with the skills and knowledge to assist employers in implementing a positive 
+      mental health culture to support employees within a workplace.
+    </p>
 
-    This course is suitable for everyone but has been designed to help employers to provide a positive mental health culture within the workplace and to provide learners with comprehensive knowledge on a range of the most common mental health conditions and the skills to be able to act should a condition be suspected.
+    <strong>Syllabus</strong>
+    <p>
+      A range of subjects are covered including:
+    </p>
 
-    Learners undertaking this course will be considered First Aiders for Mental Health and be a point of contact within the workplace to help and support those with a suspected mental health condition.
+    <ul>
+      <li>What is First Aid for Mental Health?</li>
+      <li>Identifying mental health conditions</li>
+      <li>Providing advice and starting a conversation</li>
+      <li>Drugs and alcohol</li>
+      <li>First Aid for Mental Health action plan</li>
+      <li>First Aid for Mental Health in the workplace</li>
+    </ul>
 
-    Building on the Level 1 Award in Awareness of First Aid for Mental Health this course introduces the First Aid for Mental Health Action Plan which allows learners to provide their peers with comprehensive guidance and support for a suspected mental health condition. Learners will be able to provide signposting to professional help and support that person whilst they are receiving professional assistance.
+    <strong>Level 3 Award in Supervising First Aid for Mental Health – 2 Days</strong>
+    <p>
+      This course builds on the Level 2 Award in First Aid for Mental Health and covers 
+      a wider range of mental health conditions and goes into detail on the range of 
+      therapy and professional support that a person may be given by professional bodies 
+      during treatment for a mental health condition.
+    </p>
 
-    This course also covers the effects of drugs and alcohol and provides learners with the skills and knowledge to assist employers in implementing a positive mental health culture to support employees within a workplace.
+    <p>
+      The course is suitable for all persons within a workplace but is aimed at those 
+      who hold a supervisory/managerial level position and who have responsibility for 
+      implementing a positive mental health culture and responsibility for First Aid for 
+      Mental Health within an organisation.
+    </p>
 
-    Syllabus
+    <strong>Syllabus</strong>
+  
+    <p>
+      A range of subjects are covered including:
+    </p>
+    <ul>
+      <li>What is First Aid for Mental Health?</li>
+      <li>Identifying mental health conditions</li>
+      <li>Providing advice and starting a conversation</li>
+      <li>Drugs and alcohol</li>
+      <li>First Aid for Mental Health action plan</li>
+      <li>First Aid for Mental Health in the workplace</li>
+      <li>A large range of Mental Health conditions covered in detail</li>
+    </ul>
 
-    A range of subjects are covered including:
+    <strong>Certification</strong>
+    <p>
+      Upon successful completion of the qualification assessment, Learners will be 
+      awarded the Ofqual regulated Level 3 Award in Supervising First Aid for Mental 
+      Health, Level 2 Award in First Aid for Mental Health or Level 1 Award in Awareness 
+      of First Aid for Mental Health.
+    </p>
 
-    What is First Aid for Mental Health?
+    <strong>Assessment</strong>
+    <p>
+      Summative practical assessment is ongoing by the instructor each day, along with 
+      a written assessment on each day. A Level 3 Award in Supervising First Aid for 
+      Mental Health will be issued to the learner, subject to successful assessment. No 
+      external assessors are required.
+    </p>
 
-    Identifying mental health conditions
-
-    Providing advice and starting a conversation
-
-    Drugs and alcohol
-
-    First Aid for Mental Health action plan
-
-    First Aid for Mental Health in the workplace
-
-    Level 3 Award in Supervising First Aid for Mental Health – 2 Days
-
-    This course builds on the Level 2 Award in First Aid for Mental Health and covers a wider range of mental health conditions and goes into detail on the range of therapy and professional support that a person may be given by professional bodies during treatment for a mental health condition.
-
-    The course is suitable for all persons within a workplace but is aimed at those who hold a supervisory/managerial level position and who have responsibility for implementing a positive mental health culture and responsibility for First Aid for Mental Health within an organisation.
-
-    Syllabus
-
-    A range of subjects are covered including:
-
-    What is First Aid for Mental Health?
-    Identifying mental health conditions
-    Providing advice and starting a conversation
-    Drugs and alcohol
-    First Aid for Mental Health action plan
-    First Aid for Mental Health in the workplace
-    A large range of Mental Health conditions covered in detail
-
-    Certification
-    Upon successful completion of the qualification assessment, Learners will be awarded the Ofqual regulated Level 3 Award in Supervising First Aid for Mental Health, Level 2 Award in First Aid for Mental Health or Level 1 Award in Awareness of First Aid for Mental Health.
-
-    Assessment
-    Summative practical assessment is ongoing by the instructor each day, along with a written assessment on each day. A Level 3 Award in Supervising First Aid for Mental Health will be issued to the learner, subject to successful assessment. No external assessors are required.
-
-    Numbers
-    A maximum of 12 students can be accommodated on this course and all candidates must be a minimum of 16 years of age to qualify.
+    <strong>Numbers</strong>
+    <p>
+      A maximum of 12 students can be accommodated on this course and all candidates must 
+      be a minimum of 16 years of age to qualify.
+    </p>
   </Container>
