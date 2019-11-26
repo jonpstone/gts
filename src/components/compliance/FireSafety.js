6a114e4894948913511af128c224e564b4f4b63d
Jonathan Stone <jpstone@protonmail.com>
2019-11-25 21:35:38 -0600

    Tags for JSX for Fire Safety.

--- src/components/compliance/FireSafety.js
+++ src/components/compliance/FireSafety.js
@@ -1,33 +1,43 @@
 import React from 'react'
-import { Container } from 'semantic-ui-react'
+import { Container, Header } from 'semantic-ui-react'
 
 export default props => 
   <Container text style={{ marginTop: '7em' }}>
+    <Header as='h2'>Fire Safety</Header>
+    <Header as='h5'>Level 2 Fire Safety : 1-day course</Header>
     
-Fire Safety
-Level 2 Fire Safety : 1-day course
+    <strong>
+      You will, no doubt, be well aware of the potentially devastating effects of fires 
+      in the workplace in terms of lives lost, injuries, damage to property and the 
+      environment, and to business continuity. You may also be aware that it is believed 
+      that most fires are preventable. The importance of fire safety in the workplace is 
+      underpinned by health and safety legislation that places a duty on employers to 
+      establish policies and procedures and ensure their employees receive appropriate 
+      training.
+    </strong>
 
- 
+    <strong>
+      This Fire Safety Training Course is designed for employees who want to develop 
+      their basic fire safety awareness in order to assist their employer by undertaking 
+      more specific fire safety roles in the workplace to manage fire risk (e.g. as fire 
+      wardens/marshals/stewards etc.) Successful candidates will be awarded a national 
+      level 2 qualification (Level 5 in Scotland).
+    </strong>
 
-You will, no doubt, be well aware of the potentially devastating effects of fires in the workplace in terms of lives lost, injuries, damage to property and the environment, and to business continuity. You may also be aware that it is believed that most fires are preventable. The importance of fire safety in the workplace is underpinned by health and safety legislation that places a duty on employers to establish policies and procedures and ensure their employees receive appropriate training.
+    <strong>Syllabus</strong>
+    <p>
+      A range of subjects are covered including:
+    </p>
+    
+    <ul>
+      <li>The hazards and risks associated with fire in the workplace</li>
+      <li>How fire risk is controlled in the workplace</li>
+      <li>The role of the nominated fire warden</li>
+      <li>The principles and practice of fire safety management at work</li>
+    </ul>
 
- 
-
-This Fire Safety Training Course is designed for employees who want to develop their basic fire safety awareness in order to assist their employer by undertaking more specific fire safety roles in the workplace to manage fire risk (e.g. as fire wardens/marshals/stewards etc.) Successful candidates will be awarded a national level 2 qualification (Level 5 in Scotland).
-
- 
-
-Syllabus
-
-A range of subjects are covered including:
-
-    The hazards and risks associated with fire in the workplace
-    How fire risk is controlled in the workplace
-    The role of the nominated fire warden
-    The principles and practice of fire safety management at work
-
- 
-
-Golbourn training solutions also offer a Bespoke in house Fire Safety Awareness course.
-
+    <p>
+      Golbourn training solutions also offer a Bespoke in house Fire Safety Awareness 
+      course.
+    </p>
   </Container>
