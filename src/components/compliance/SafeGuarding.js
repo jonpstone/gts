59a1624fd89cdec03150545fc2c4f1c5038200ae
Jonathan Stone <jpstone@protonmail.com>
2019-12-02 16:27:15 -0600

    Added JSX with HTML tags

--- src/components/compliance/SafeGuarding.js
+++ src/components/compliance/SafeGuarding.js
@@ -3,5 +3,60 @@ import { Container } from 'semantic-ui-react'
 
 export default props => 
   <Container text style={{ marginTop: '7em' }}>
-    Safeguarding
+    <p>
+      <strong>Safeguarding is how we can protect children, young people and vulnerable
+        adults from abuse or neglect by the actions, or lack of actions, of another 
+        person. In order to safeguard and protect persons from abuse or neglect we need
+        to learn how to identify vulnerable individuals and take necessary steps to stop
+        abuse and neglect.
+      </strong>
+    </p>     
+
+    <p>
+      Everyone has the right to be safe and live without the fear of abuse, neglect 
+      or exploitation and this course aims to teach you how to recognise the signs of
+      abuse and neglect and what actions you can take to stop this from happening and 
+      occurring again in the future. Abuse, neglect and exploitation can occur anywhere
+      and by anyone, quite often being a person of trust.
+    </p>
+
+    <p>
+      This course is designed for all persons, no matter the field that they are 
+      directly involved in, to give them the tools and knowledge to make a difference 
+      to a vulnerable person’s life. It will also let the practitioner reporting the 
+      abuse know how they can safeguard themselves in every day work, while seeking 
+      help and reporting the incident identified.
+    </p>
+
+    <strong>Syllabus</strong>
+    
+    <strong>A range of subjects are covered including:</strong>
+    <ul>
+      <li>Safeguarding legislation and guidance</li>
+      <li>Indictors of abuse or neglect</li>
+      <li>Making judgements</li>
+      <li>Communication</li>
+      <li>Roles and responsibilities</li>
+      <li>Sharing of information</li>
+      <li>Dealing with and reporting of allegations and complaints</li>
+      <li>Assessment and marking</li>
+    </ul>
+    
+    <strong>Certification</strong>
+    <p>
+      Upon successful completion of the qualification assessment, Learners will be 
+      awarded an Ofqual national Level 3 Award.
+    </p>
+
+    <strong>Numbers</strong>
+    <p>
+      A maximum of 12 students can be accommodated on this course and all candidates 
+      must be a minimum of 18 years of age to qualify.
+    </p>
+
+    <strong>Assessment</strong>
+    <p>
+      A range of methods are used, including, practical and questioning (open and 
+      multiple-choice).
+    </p>
   </Container>
