9b55bde026ee784d43d54c5c96884cfbf9ae83a9
Jonathan Stone <jpstone@protonmail.com>
2019-12-02 21:38:58 -0600

    HTML tags for BlsAed

--- src/components/compliance/BlsAed.js
+++ src/components/compliance/BlsAed.js
@@ -3,38 +3,70 @@ import { Container } from 'semantic-ui-react'
 
 export default props => 
   <Container text style={{ marginTop: '7em' }}>
-    Heart and circulatory disease claims around 200,000 lives each year. Many could have been saved if a defibrillator was made available.
+    <p>
+      Heart and circulatory disease claims around 200,000 lives each year. Many could 
+      have been saved if a defibrillator was made available.
+    </p>
 
-    Automated External Defibrillators (AED’s) are now becoming more common throughout a range of public and work places. A casualty’s survival rate will dramatically increase if an AED is available and if you have appropriately qualified personnel to use it.
+    <p>
+      Automated External Defibrillators (AED’s) are now becoming more common throughout
+      a range of public and work places. A casualty’s survival rate will dramatically 
+      increase if an AED is available and if you have appropriately qualified personnel 
+      to use it.
+    </p>
 
-    This very practical course will enable your learners to use the AED competently and with confidence.
+    <p>
+      This very practical course will enable your learners to use the AED competently 
+      and with confidence.
+    </p>
 
-    Not only does this course deal with the use of an AED but it also includes vital training in resuscitation and choking procedures.
+    <p>
+      Not only does this course deal with the use of an AED but it also includes vital
+      training in resuscitation and choking procedures.
+    </p>
 
-    Course Duration
+    <strong>Course Duration</strong>
 
-    A minimum of six contact hours in the classroom, consisting of practical and theoretical activities.
+    <p>
+      A minimum of six contact hours in the classroom, consisting of practical and 
+      theoretical activities.
+    </p>
 
-    Syllabus
+    <strong>Syllabus</strong>
 
-    A range of subjects are covered including:
+    <strong>A range of subjects are covered including:</strong>
+      <ul>
+        <li>Responsibilities and reporting</li>
+        <li>Dealing with an unresponsive casualty</li>
+        <li>Assessment of the situation</li>
+        <li>Paediatric defibrillation</li>
+        <li>Defibrillation procedures</li>
+        <li>Resuscitation</li>
+        <li>Choking</li>
+        <li>Heart attacks</li>
+        <li>AED safety and Functions</li>
+        <li>Infection control</li> 
+      </ul>
 
-        Responsibilities and reporting
-        Dealing with an unresponsive casualty
-        Assessment of the situation
-        Paediatric defibrillation
-        Defibrillation procedures
-        Resuscitation
-        Choking
-        Heart attacks
-        AED safety and Functions
-        Infection control 
+    <li>
+      CPR and AED training is fast becoming a necessity. Golbourn Training Solutions 
+      not only offer this dedicated CPR and AED training course but also a 4 hour 
+      Basic Life Support and AED course.
+    </li>
 
-    CPR and AED training is fast becoming a necessity. Golbourn Training Solutions not only offer this dedicated CPR and AED training course but also a 4 hour Basic Life Support and AED course.
+    <p>
+      Each student will also receive a free comprehensive fully illustrated training 
+      book to supplement the CPR and AED course this will be provided in your training
+      pack. 
+    </p>
 
-    Each student will also receive a free comprehensive fully illustrated training book to supplement the CPR and AED course this will be provided in your training pack. 
+    <p>
+      A maximum of 12 students are allowed and must be a minimum of 16 years of age.
+    </p>
 
-    A maximum of 12 students are allowed and must be a minimum of 16 years of age.
-
-    This qualification lasts for 3 years. To renew their qualification after 3 years, delegates will need to retake the full course. However, it is recommended that, during the 3 year period, delegates attend annual refresher courses.
+    <p>
+      This qualification lasts for 3 years. To renew their qualification after 3 
+      years, delegates will need to retake the full course. However, it is recommended
+       that, during the 3 year period, delegates attend annual refresher courses.
+    </p>    
   </Container>
