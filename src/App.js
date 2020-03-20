5f2f08ce9f7ac732162d06f77d7c98971d609985
Jonathan Stone <jpstone@protonmail.com>
2020-03-20 17:09:43 -0500

    Used JS and CSS trick for vh sizing.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import AppRouter from './routers/AppRouter';
 import 'semantic-ui-css/semantic.min.css'
 import './styles/main.scss';
 
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 function App() {
 	return (
 		<div className="App">
